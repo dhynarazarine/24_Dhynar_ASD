c8499d1a7162b2899b6155b30d4f3118e3bbbbef
dhynarazarine <144086807+dhynarazarine@users.noreply.github.com>
2024-02-15 16:11:21 +0700

    Add files via upload

---
+++ Pertemuan 1/Array_24.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Array_24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("=========================");
+        System.out.println("Program Menghitung IP Semester");
+        System.out.println("=========================");
+        
+        // Nama-nama mata kuliah
+        String[] matkul = {"Pancasila", "Konsep Teknologi Informasi", "Critical Thinking and Problem Solving", "Matematika Dasar", "Bahasa Inggris", "Dasar Pemrograman", "Praktikum Dasar Pemrograman", "Keselamatan dan Kesehatan Kerja"};
+       
+        // Bobot SKS untuk setiap mata kuliah
+        int[] sks= {2, 2, 3, 3, 3, 4, 2, 2};
+        double[] nilaiAngka = new double[8];
+        String[] nilaiHuruf = new String[8];
+        double[] bobotNilai = new double[8];
+        double ip, totalIP = 0, ipSemester;
+        int totalSKS = 0;
+
+        // Memasukkan nilai angka untuk setiap mata kuliah
+        for (int i = 0; i < matkul.length; i++) {
+            System.out.print("Masukkan nilai Angka untuk MK " + matkul[i] + ": ");
+            nilaiAngka[i] = scanner.nextDouble();
+            
+            if(nilaiAngka[i] <= 100 && nilaiAngka[i] > 80) {
+                nilaiHuruf[i] = "A";
+                bobotNilai[i] = 4;
+            } else if (nilaiAngka[i] <= 80 && nilaiAngka[i] > 73){
+                nilaiHuruf[i] = "B+";
+                bobotNilai[i] = 3.5;
+            }  else if (nilaiAngka[i] <= 73 && nilaiAngka[i] > 65){
+                nilaiHuruf[i] = "B";
+                bobotNilai[i] = 3;
+            }  else if (nilaiAngka[i] <= 65 && nilaiAngka[i] > 60){
+                nilaiHuruf[i] = "C+";
+                bobotNilai[i] = 2.5;
+            }  else if (nilaiAngka[i] <= 60 && nilaiAngka[i] > 50){
+                nilaiHuruf[i] = "C";
+                bobotNilai[i] = 2;
+            }  else if (nilaiAngka[i] <= 50 && nilaiAngka[i] > 39){
+                nilaiHuruf[i] = "D";
+                bobotNilai[i] = 1;
+            }  else if (nilaiAngka[i] <= 39){
+                nilaiHuruf[i] = "E";
+                bobotNilai[i] = 0;
+            } if (nilaiAngka[i] > 100 && nilaiAngka[i] < 1) {
+                nilaiAngka[i] = 0;
+                nilaiHuruf[i] = "Tidak Valid!";
+                bobotNilai[i] = 0;
+            }
+        }
+
+        System.out.println("=====================");
+        System.out.println("Hasil Konversi Nilai");
+        System.out.println("=====================");
+        System.out.printf();
+        
+         
+    }
+}
