f625e5848b2092c0d27a89b8b1009b18c225d091
dhynarazarine <dinar.andiyani@gmail.com>
2024-04-04 22:50:12 +0700

    Tugas kuliah. (Modif dan Tugas 1)

---
+++ Pertemuan 9/Jobsheet 7/Struk24.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class Struk24 {
+    int nomorTransaksi;
+    String tanggalPembelian;
+    int jumlahBarang;
+    double totalHargaBayar;
+
+    public Struk24(int nomorTransaksi, String tanggalPembelian, int jumlahBarang, double totalHargaBayar) {
+        this.nomorTransaksi = nomorTransaksi;
+        this.tanggalPembelian = tanggalPembelian;
+        this.jumlahBarang = jumlahBarang;
+        this.totalHargaBayar = totalHargaBayar;
+    }
+
+    @Override
+    public String toString() {
+        return "Nomor Transaksi: " + nomorTransaksi +
+                "\nTanggal Pembelian: " + tanggalPembelian +
+                "\nJumlah Barang: " + jumlahBarang +
+                "\nTotal Harga Bayar: " + totalHargaBayar + "\n";
+    }
+
+}
---
+++ Pertemuan 9/Jobsheet 7/StrukMain24.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StrukMain24{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Stack<Struk24> stack = new Stack<>();
+
+        System.out.println("====================================");
+        System.out.println("| Masukkan informasi struk belanja |");
+        System.out.println("====================================");
+        for (int i = 0; i < 8; i++) {
+            System.out.println("Struk Belanja ke-" + (i + 1) + ":");
+            System.out.print("Nomor Transaksi  : ");
+            int nomorTransaksi = scanner.nextInt();
+            scanner.nextLine(); // Membersihkan newline
+            System.out.print("Tanggal Pembelian: ");
+            String tanggalPembelian = scanner.nextLine();
+            System.out.print("Jumlah Barang    : ");
+            int jumlahBarang = scanner.nextInt();
+            System.out.print("Total Harga Bayar: ");
+            double totalHargaBayar = scanner.nextDouble();
+            scanner.nextLine(); // Membersihkan newline
+
+            Struk24 struk = new Struk24(nomorTransaksi, tanggalPembelian, jumlahBarang, totalHargaBayar);
+            stack.push(struk);
+        }
+        System.out.println("====================================");
+
+        System.out.println("\n================================================");
+        System.out.println("| Informasi struk belanja yang masih tersimpan |");
+        System.out.println("================================================");
+        for (Struk24 struk : stack) {
+            System.out.println(struk);
+            System.out.println("================================================");
+        }
+
+        System.out.println("\n=======================================================");
+        System.out.println("| Mengambil lima struk belanja untuk ditukarkan       |");
+        System.out.println("| dengan voucher belanja:                             |");
+        System.out.println("=======================================================");
+        for (int i = 0; i < 5; i++) {
+            Struk24 strukDitukar = stack.pop();
+            System.out.println("Struk yang ditukarkan ke-" + (i + 1) + ":\n" + strukDitukar);
+            System.out.println("=======================================================");
+        }
+
+        scanner.close();
+    }
+}
