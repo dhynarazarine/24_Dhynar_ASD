b641ed2d8f402281b236aa646912c08465ff0347
dhynarazarine <dinar.andiyani@gmail.com>
2024-05-30 10:53:39 +0700

    Tugas Kuliah

---
+++ Pertemuan 15/Jobsheet 12/Graph/GraphMain24.java
@@ -0,0 +1,13 @@
+public class GraphMain24 {
+    public static void main(String[] args) throws Exception{
+        Graph24 gedung = new Graph24(6);
+        gedung.addEdge(0, 1, 50);
+        gedung.addEdge(0, 2, 100);
+        gedung.addEdge(1, 3, 70);
+        gedung.addEdge(2, 3, 40);
+        gedung.addEdge(3, 4, 60);
+        gedung.addEdge(4, 5, 80);
+        gedung.degree(0);
+        gedung.printGraph();
+    }
+}
